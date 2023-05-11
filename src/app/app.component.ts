67cbc2f3f154671d815d6377bab294d6b310bf6d
naoya.sase <sasenaoya@gmail.com>
2023-05-11 09:40:03 +0000

    とりあえずPDFを読み込んでみる（でも読み込み時にエラーが発生した！）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import * as pdfjs from 'pdfjs-dist';
 
+/**
+ * とりあえずPDFを読み込んでみるサンプル。
+ * でも、読み込み時に以下のエラーが発生する。
+ * No "GlobalWorkerOptions.workerSrc" specified.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdfjs-test';
+  pdf?: pdfjs.PDFDocumentProxy;
+
+  /** PDFファイルが選択された */
+  onPdfFileChange(e: Event) {
+    const files = (e.target as any).files;
+    if (files?.length > 0) {
+      this.openPdf(files[0]);
+    }
+  }
+
+  /** PDFファイルを読み込む */
+  private openPdf(file: File) {
+    var fileReader = new FileReader();
+    fileReader.onload = () => {
+      const loadingTask = pdfjs.getDocument(fileReader.result!);
+      loadingTask.promise.then(pdf => {
+        this.pdf = pdf;
+      });
+    };
+    fileReader.readAsArrayBuffer(file);
+  }
 }
