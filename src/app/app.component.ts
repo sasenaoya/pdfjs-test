b52a1f152c287e2dc8fe88916af4e76671986a1f
naoya.sase <sasenaoya@gmail.com>
2023-05-11 09:55:50 +0000

    PDFを描画してみる（でもフォントが描画されない！）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as pdfjs from 'pdfjs-dist';
 
 /**
  * とりあえずPDFを読み込んでみるサンプル。
- * 読み込んだPDFのページ数を表示します。
+ * 読み込んだPDFをCanvasに描画してみる。
+ * でもPDFによってはフォントが描画されない。
  */
 @Component({
   selector: 'app-root',
@@ -11,8 +12,12 @@ import * as pdfjs from 'pdfjs-dist';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('pdfCanvas') pdfCanvas?: ElementRef;
   title = 'pdfjs-test';
   pdf?: pdfjs.PDFDocumentProxy;
+  pageNumber?: number;
+  canvasWidth: number = 0;
+  canvasHeight: number = 0;
 
   constructor() {
     pdfjs.GlobalWorkerOptions.workerSrc = '/assets/pdfjs/pdf.worker.min.js';
@@ -27,6 +32,31 @@ export class AppComponent {
     }
   }
 
+  /** PDFをCanvasに描画する */
+  async renderPage(pageNumber: number) {
+    if (this.pdf && this.pdfCanvas) {
+      this.pageNumber = pageNumber;
+
+      const page = await this.pdf.getPage(pageNumber);
+      const scale = 4.0;
+      const viewport = page.getViewport({ scale });
+
+      const canvas = this.pdfCanvas.nativeElement;
+      canvas.width = Math.floor(viewport.width);
+      canvas.height = Math.floor(viewport.height);
+      this.canvasWidth = canvas.width / scale;
+      this.canvasHeight = canvas.height / scale;
+
+      const context = canvas.getContext("2d");
+
+      const renderContext = {
+        canvasContext: context,
+        viewport,
+      };
+      page.render(renderContext);
+    }
+  }
+
   /** PDFファイルを読み込む */
   private openPdf(file: File) {
     var fileReader = new FileReader();
@@ -34,6 +64,10 @@ export class AppComponent {
       const loadingTask = pdfjs.getDocument(fileReader.result!);
       loadingTask.promise.then(pdf => {
         this.pdf = pdf;
+        setTimeout(() => {
+          // pdfが設定されないとcanvasが作成されないので、setTimeoutで待ってから描画する
+          this.renderPage(1);
+        });
       });
     };
     fileReader.readAsArrayBuffer(file);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
